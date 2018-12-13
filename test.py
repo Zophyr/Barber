c479e6a21154dca32d0f3778dbf65ca1cb88227f
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2018-12-13 19:09:32 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from barber import barber
+
+str = "e36bc283b3ae643d85ba28ae12d09f61b48dd3e9f7f816ad692d60de96d0df59dbc3ace23348ab1a6a1dcd0e9840b733"
+
+print(barber.cut(str))
