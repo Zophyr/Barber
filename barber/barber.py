cc32cf6d68573e33f719b2193b6e6e6fdd984d60
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2018-12-13 19:09:29 +0800

    Create barber.py

---
+++ barber/barber.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+    barber
+    ~~~~~~~~~~~~~
+
+    a useless tool to cut the string with blank and line break.
+"""
+
+def cut(str, length = 4,  group = 8, style = ' '):
+    
+    str = "".join([str[i] + style if (i  + 1) % length == 0 and not i == 0 else str[i] for i in range(len(str))])
+    str = "".join([str[i] + "\n" if (i  + 1) % ((length + 1) * group) == 0 and not i == 0 else str[i] for i in range(len(str))])
+
+    return str
