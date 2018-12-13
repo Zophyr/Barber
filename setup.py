1a2bdc33120a340153800073a3ff8cc259c03698
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2018-12-13 22:41:11 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='barber',
+      version='0.0.2',
+      description='Cut the string with blank and line break.',
+      author='Zophyr',
+      author_email='the-zephyr@hotmail.com',
+      url='https://zophyr.com',
+      packages=['barber'],
+     )
